7ce7840b6335ec93c6e3f091feebd3f94667c971
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-10-16 18:35:31 +0200

    Also install additional bashrc stuff

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 mydir=$(readlink -f "$(dirname $0)")
 
+echo Creating symlinks in '`'$HOME"':"
 for i in "$(dirname $0)"/dotfiles/*; do
-    ln -s "$mydir/dotfiles/$(basename $i)" "$HOME/.$(basename $i)"
+    echo -n '-> `'$(basename $i)"' "
+    if [ ! -e "$HOME/.$(basename $i)" ]; then
+        ln -s "$mydir/dotfiles/$(basename $i)" "$HOME/.$(basename $i)"
+        echo "done."
+    else
+        echo "already exists."
+    fi
 done
+
+echo Installing to '`'$HOME/.bashrc"'"...
+grep -F "bashrc.dotfiles" "$HOME/.bashrc" || (echo -e '\n# Added by leod/dotfiles/install.sh\nsource ~/.bashrc.dotfiles' >> "$HOME/.bashrc")
+
+echo Done.
